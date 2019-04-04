acbf0c1a7e1e41c356f5e0e62800987b96a589dd
Sandra Cabrera <sandra.cabrera@ETOOL.TECH>
2019-04-04 12:25:07 +0200

    corrección del error de nombre duplicado

--- app/__init__.py
+++ app/__init__.py
@@ -15,8 +15,6 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models, errors
-
 if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
@@ -29,18 +27,20 @@ if not app.debug:
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr='no-reply@' + app.config['MAIL_SERVER'],
             toaddrs=app.config['ADMINS'], subject='Microblog Failure',
-            credentials=auth, secure=secure
-        )
-    mail_handler.setLevel(logging.ERROR)
-    app.logger.addHandler(mail_handler)
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
     file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
-    ))
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+from app import routes, models, errors

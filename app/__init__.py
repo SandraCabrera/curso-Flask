3c51144e4c3c033a78e27f47972458c70a5f2ee1
sandra.cabrera <sandracabreragarcia26@gmail.com>
2019-04-09 12:58:13 +0200

    Selecciona el mejor idioma

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -53,3 +53,7 @@ if not app.debug:
     app.logger.info('Microblog startup')
 
 from app import routes, models, errors
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])

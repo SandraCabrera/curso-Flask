3193049ef5762c76fd7ccae79c7262d215b14a77
sandra.cabrera <sandracabreragarcia26@gmail.com>
2019-04-11 11:08:32 +0200

    bug corregido main

--- microblog.py
+++ microblog.py
@@ -1,10 +1,11 @@
 from app.models import User, Post
 from app import cli, create_app, db
 
-app.run()
 app = create_app()
 cli.register(app)
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+
+app.run()

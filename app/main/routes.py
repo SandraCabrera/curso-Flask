b871a44c440856b0ed5de2749b8a7608b0898da5
sandra.cabrera <sandracabreragarcia26@gmail.com>
2019-04-11 10:53:21 +0200

    Refactorización

--- app/main/routes.py
+++ app/main/routes.py
@@ -3,7 +3,6 @@ from flask import render_template, flash, redirect, url_for, request, g, \
     jsonify, current_app
 from flask_login import current_user, login_required
 from flask_babel import _, get_locale
-from guess_language import guess_language
 from app import db
 from app.main.forms import EditProfileForm, PostForm
 from app.models import User, Post

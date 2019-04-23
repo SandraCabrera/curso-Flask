fec3e875b44d7363034b9a4d098e2e5a6ededfeb
sandra.cabrera <sandracabreragarcia26@gmail.com>
2019-04-23 12:23:42 +0200

    Entrega de notificaciones a los clientes

--- app/main/routes.py
+++ app/main/routes.py
@@ -8,6 +8,7 @@ from app.main.forms import EditProfileForm, PostForm, MessageForm
 from app.models import User, Post, Message
 from app.translate import translate
 from app.main import bp
+from app.models import Notification
 
 
 @bp.before_app_request
@@ -165,3 +166,15 @@ def messages():
         if messages.has_prev else None
     return render_template('messages.html', messages=messages.items,
                            next_url=next_url, prev_url=prev_url)
+
+@bp.route('/notifications')
+@login_required
+def notifications():
+    since = request.args.get('since', 0.0, type=float)
+    notifications = current_user.notifications.filter(
+        Notification.timestamp > since).order_by(Notification.timestamp.asc())
+    return jsonify([{
+        'name': n.name,
+        'data': n.get_data(),
+        'timestamp': n.timestamp
+    } for n in notifications])

327b1733b7c631de97eec3256298a2fd8309b1a6
sandra.cabrera <sandracabreragarcia26@gmail.com>
2019-04-23 13:01:49 +0200

    redis añadido config

--- microblog.py
+++ microblog.py
@@ -1,12 +1,11 @@
-from app.models import User, Post, Notification, Message, Task
-from app import cli, create_app, db
+from app import create_app, db, cli
+from app.models import User, Post, Message, Notification, Task
 
 app = create_app()
 cli.register(app)
+
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
             'Notification': Notification, 'Task': Task}
-
-app.run()

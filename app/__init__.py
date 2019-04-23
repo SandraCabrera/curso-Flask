0639feaa6c60946d3eb6054427c5bc0af1995f94
sandra.cabrera <sandracabreragarcia26@gmail.com>
2019-04-23 12:55:05 +0200

    Implementando la tarea de exportación

--- app/__init__.py
+++ app/__init__.py
@@ -9,9 +9,9 @@ from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
-from config import Config
 from redis import Redis
 import rq
+from config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()

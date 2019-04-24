959f09cf10486efe5d14f4fa7ff906714f230b7d
sandra.cabrera <sandracabreragarcia26@gmail.com>
2019-04-24 11:45:36 +0200

    Registro de nuevos usuarios

--- app/api/users.py
+++ app/api/users.py
@@ -1,6 +1,8 @@
 from app.api import bp
-from flask import jsonify, request
+from flask import jsonify, request, url_for
 from app.models import User
+from app import db
+from app.api.errors import bad_request
 
 @bp.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
@@ -31,7 +33,21 @@ def get_followed(id):
 
 @bp.route('/users', methods=['GET'])
 def create_user():
-    pass
+    data = request.get_json() or {}
+    if 'username' not in data or 'email' not in data or 'password' not in data:
+        return bad_request('must include username, email and password fields')
+    if User.query.filter_by(username=data['username']).first():
+        return bad_request('please use a different username')
+    if User.query.filter_by(email=data['email']).first():
+        return bad_request('please use a different email address')
+    user = User()
+    user.from_dict(data, new_user=True)
+    db.session.add(user)
+    db.session.commit()
+    response = jsonify(user.to_dict())
+    response.status_code = 201
+    response.headers['Location'] = url_for('api.get_user', id=user.id)
+    return response
 
 @bp.route('/users/<int:id>', methods=['GET'])
 def update_user(id):
